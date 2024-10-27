bd3fccb3196993d8bbab9a1f809bcb27b01c9724
Ashmit0405 <173537273+Ashmit0405@users.noreply.github.com>
2024-10-27 20:23:49 +0530

    Added the flask api

---
+++ backend/Flask_server/server.py
@@ -0,0 +1,28 @@
+# server.py
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+from scipy.sparse import hstack
+app = Flask(__name__)
+
+model = joblib.load('./model/name_to_gender_detector.pkl')
+tokenizer=joblib.load('./model/tokenizer.pkl')
+scaler=joblib.load('./model/scaler.pkl')
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    name = data.get('name', '')
+    count = data.get('count', 0)
+    probability = data.get('probability', 0.0)
+
+    name_processed = tokenizer.transform([name])
+    count_probability_processed = scaler.transform(np.array([[count, probability]]))
+    x_input = hstack([name_processed, count_probability_processed])
+
+    prediction = model.predict(x_input)
+    gender = 'M' if prediction[0] == 1 else 'F'
+
+    return jsonify({'gender': gender})
+
+if __name__ == '__main__':
+    app.run(port=5000)
